2dd737efb7ded0b4bbd624a313bdbe0b06f9012a
Aaron Polley <aarondavidpolley@gmail.com>
2018-01-30 01:23:23 +1100

    Better Comments

--- UserHome-FUT-CopyToUserTemplate.sh
+++ UserHome-FUT-CopyToUserTemplate.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
 
-FILE="/Library/Preferences/com.squirrels.Reflection.plist"
+#####################################################
+#Script for copying files to User Templates.        #
+#####################################################
+
+#####################################################
+#Install the file to /tmp & specify the file details#
+#####################################################
+#Edit Below#                                        #
+#####################################################
+
+#Name of file - including extension#
+FILE="com.github.aarondavidpolley.plist"
+
+#Desired Subfolder to copy file in the User Template Folders - i.e. What comes after /Users/<username> when created#
+SUBFOLDER="/Library/Preferences/"
+
+#Folder to copy the file from#
+TMPFOLDER="/tmp/"
+
+#####################################################
+#DON't EDIT AFTER THIS LINE                         #
+#####################################################
+
+
+COPYPATH="${TMPFOLDER}${FILE}"
+
 
 for USER_TEMPLATE in "/System/Library/User Template"/*
   do
-    cp "${FILE}" "${USER_TEMPLATE}"/Library/Preferences/
+    cp "${COPYPATH}" "${USER_TEMPLATE}${SUBFOLDER}"
   done
+
+
