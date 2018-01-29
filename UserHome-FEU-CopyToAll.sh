2ada1ca59e5d65fb77e0f216169650546284ca0d
Aaron Polley <aarondavidpolley@gmail.com>
2018-01-30 01:10:23 +1100

    Modular Update
    More modular FEU script

--- UserHome-FEU-CopyToAll.sh
+++ UserHome-FEU-CopyToAll.sh
@@ -1,15 +1,39 @@
 #!/bin/bash
 
+#####################################################
+#Install the file to /tmp & specify the file details#
+#####################################################
+#Edit Below#                                        #
+#####################################################
+
+#Folder containing User Folders#
+#System Default below#
 UHOME="/Users/"
-FILE="/Library/Preferences/com.squirrels.Reflection.plist"
+
+#Name of file - including extension#
+FILE="com.github.aarondavidpolley.plist"
+
+#Desired Subfolder to copy file in the User Folders - i.e. What comes after /Users/<username>#
+SUBFOLDER="/Library/Preferences/"
+
+#Folder to copy the file from#
+TMPFOLDER="/tmp/"
+
+#####################################################
+#DON't EDIT AFTER THIS LINE                         #
+#####################################################
+
+
+COPYPATH="${TMPFOLDER}${FILE}"
+
  # get list of all users
 _USERS="$(dscl . list /Users | grep -v ^_.*)"
 for u in $_USERS
 do
-   _dir="${UHOME}/${u}/Library/Preferences/"
+   _dir="${UHOME}/${u}${SUBFOLDER}"
    if [ -d "$_dir" ]
    then
-       /bin/cp "$FILE" "$_dir"
-      chown $(id -un $u):$(id -gn $u) "$_dir/${FILE}"
+       /bin/cp "$COPYPATH" "$_dir"
+      chown $(id -un $u):$(id -gn $u) "$_dir${FILE}"
    fi
 done
