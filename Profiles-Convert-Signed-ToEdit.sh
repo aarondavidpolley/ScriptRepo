90288a079bcb48024a7f9eb99719d7d419e855e8
Aaron Polley <aarondavidpolley@gmail.com>
2018-04-25 19:37:56 +1000

    10.13 Changes

--- Profiles-Convert-Signed-ToEdit.sh
+++ Profiles-Convert-Signed-ToEdit.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
 #Convert Signed/Encrypted profiles to unsigned so they can be edited#
+#Pre 10.13#
 
 /usr/bin/security cmd -D -I /path/to/signed_profile.mobileconfig -o /path/to/unsigned_profile.mobileconfig
+
+#post 10.13#
+
+/usr/bin/security cms -D -i /path/to/signed_profile.mobileconfig -o /path/to/unsigned_profile.mobileconfig
+
+#Make more human readable formatting#
+
+/usr/bin/plutil -convert xml1 /path/to/unsigned_profile.mobileconfig
