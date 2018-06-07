a8cf95c497bf4621ff345a920b99b529f211c706
Aaron Polley <aarondavidpolley@gmail.com>
2018-06-07 10:52:49 +1000

    Added no restarts

--- Dockutil-Add-Items.sh
+++ Dockutil-Add-Items.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-/usr/local/bin/dockutil --add /Applications/Microsoft\ Word.app/ --allhomes
-/usr/local/bin/dockutil --add /Applications/Microsoft\ Excel.app/ --allhomes
-/usr/local/bin/dockutil --add /Applications/Microsoft\ PowerPoint.app/ --allhomes
-/usr/local/bin/dockutil --add /Applications/Managed\ Software\ Center.app/ --after 'Launchpad' --allhomes
+/usr/local/bin/dockutil --add /Applications/Microsoft\ Word.app/ --allhomes --no-restart
+/usr/local/bin/dockutil --add /Applications/Microsoft\ Excel.app/ --allhomes --no-restart
+/usr/local/bin/dockutil --add /Applications/Microsoft\ PowerPoint.app/ --allhomes --no-restart
+/usr/local/bin/dockutil --add /Applications/Microsoft\ OneNote.app/ --allhomes --no-restart
+/usr/local/bin/dockutil --add /Applications/Microsoft\ Outlook.app/ --allhomes
+/usr/local/bin/dockutil --add /Applications/Managed\ Software\ Center.app/ --position 2 --allhomes
