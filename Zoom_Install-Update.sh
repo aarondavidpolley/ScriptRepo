4a941dd5178fb97be7cf5dec3bbdc4e41ab2f9c1
Aaron Polley <aarondavidpolley@gmail.com>
2020-01-30 10:16:51 +1000

    Create Zoom_Install-Update.sh

---
+++ Zoom_Install-Update.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+zoomURL="https://zoom.us/client/latest/Zoom.pkg"
+zoomDownload=$(/usr/bin/curl -I https://zoom.us/client/latest/Zoom.pkg | grep "location" | cut -d':' -f2- | awk '{print $1}')
+zoomVers=$(echo "${zoomDownload}" | rev | cut -d'/' -f2- | cut -d'/' -f1 | rev)
+zoomPKG="Zoom-${zoomVers}.pkg"
+
+if [ -e /Applications/zoom.us.app/Contents/Info.plist ]
+then
+	echo "Zoom is installed, checking version"
+	zoomVersLocal=$(defaults read /Applications/zoom.us.app/Contents/Info.plist CFBundleVersion)
+	echo "Local version is ${zoomVersLocal}"
+else
+	echo "Zoom is not installed in Applications folder"	
+fi
+
+echo "Latest Zoom version is ${zoomVers}"
+
+if [ -e /Applications/zoom.us.app/Contents/Info.plist ]
+then
+	if [ "${zoomVers}" = "${zoomVersLocal}" ]
+	then
+		echo "Latest version is already installed, exiting"
+		exit 0
+	fi
+fi
+
+echo "Downloading latest zoom version"
+/usr/bin/curl --location "${zoomURL}" -o "/tmp/${zoomPKG}"
+
+/usr/sbin/installer -pkg "/tmp/${zoomPKG}" -target /
+
+if [ -e /Applications/zoom.us.app/Contents/Info.plist ]
+then
+	zoomVersLocal=$(defaults read /Applications/zoom.us.app/Contents/Info.plist CFBundleVersion)
+	if [ "${zoomVers}" !=  "${zoomVersLocal}" ]
+	then
+		echo "Upgrade failed, exiting"
+		exit 1
+	fi
+else
+	echo "Install failed, exiting"
+	exit 1
+fi
+
+echo "Removing tmp file /tmp/${zoomPKG}"
+rm -rf "/tmp/${zoomPKG}"
+
+exit 0
