c03fce0b45e3d1a357144a75acd2a9bbdb9b79dc
Aaron Polley <aarondavidpolley@gmail.com>
2020-03-16 14:55:58 +1000

    Update JamfPro-Casper-EA-Firmware_Password_Status.sh

--- JamfPro-Casper-EA-Firmware_Password_Status.sh
+++ JamfPro-Casper-EA-Firmware_Password_Status.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
 #Purpose - Jamf Pro EA to check if firmware password is set
+#Source - https://github.com/aarondavidpolley/ScriptRepo/blob/master/JamfPro-Casper-EA-Firmware_Password_Status.sh
 
 WHOAREYOU=$(whoami)
 
 if [ "$WHOAREYOU" != "root" ]
 then
-	echo "<result>SCRIPT ERROR</result>"
+	echo "<result>SCRIPT ERROR - Not Root</result>"
+	exit 1
+fi
+
+if [ ! -e /usr/sbin/firmwarepasswd ]
+then
+	echo "<result>SCRIPT ERROR - Binary Missing</result>"
 	exit 1
 fi
 
@@ -15,7 +22,7 @@ Firmware_Pass_Status=$(/usr/sbin/firmwarepasswd -check | cut -d":" -f2- | cut -d
 
 if [ -z "${Firmware_Pass_Status}" ]
 then
-	echo "<result>SCRIPT ERROR</result>"
+	echo "<result>SCRIPT ERROR - Empty Output</result>"
 	exit 1
 else
 	echo "<result>${Firmware_Pass_Status}</result>"
