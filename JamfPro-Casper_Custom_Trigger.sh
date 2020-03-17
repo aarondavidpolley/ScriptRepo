f35b9b6f25846864e0eb5a9b0f1a2926e5f31e8d
Aaron Polley <aarondavidpolley@gmail.com>
2020-03-18 00:49:52 +1000

    Update JamfPro-Casper_Custom_Trigger.sh

--- JamfPro-Casper_Custom_Trigger.sh
+++ JamfPro-Casper_Custom_Trigger.sh
@@ -1,6 +1,46 @@
 ﻿#!/bin/sh
 
 #Designed to call custom triggers from other policies in Jamf Pro
-#Make sure to label Variable 4 in Jamf Pro as "Custom Trigger"
+#Call a single or set of custom triggers, helpful in deployments or for grouped self service policies
 
-/usr/local/jamf/bin/jamf policy -trigger "${4}"
+if [ ! -z "${4}" ]
+then
+	/usr/local/jamf/bin/jamf policy -trigger "${4}"
+fi
+
+if [ ! -z "${5}" ]
+then
+	/usr/local/jamf/bin/jamf policy -trigger "${5}"
+fi
+
+if [ ! -z "${6}" ]
+then
+	/usr/local/jamf/bin/jamf policy -trigger "${6}"
+fi
+
+if [ ! -z "${7}" ]
+then
+	/usr/local/jamf/bin/jamf policy -trigger "${7}"
+fi
+
+if [ ! -z "${8}" ]
+then
+	/usr/local/jamf/bin/jamf policy -trigger "${8}"
+fi
+
+if [ ! -z "${9}" ]
+then
+	/usr/local/jamf/bin/jamf policy -trigger "${9}"
+fi
+
+if [ ! -z "${10}" ]
+then
+	/usr/local/jamf/bin/jamf policy -trigger "${10}"
+fi
+
+if [ ! -z "${11}" ]
+then
+	/usr/local/jamf/bin/jamf policy -trigger "${11}"
+fi
+
+exit 0
