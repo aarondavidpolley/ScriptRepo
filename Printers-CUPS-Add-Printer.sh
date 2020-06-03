4be0bcdeb80ab4accb0d670adb16bb240b82f155
Aaron Polley <aarondavidpolley@gmail.com>
2020-06-03 10:17:21 +1000

    Update Printers-CUPS-Add-Printer.sh

--- Printers-CUPS-Add-Printer.sh
+++ Printers-CUPS-Add-Printer.sh
@@ -2,18 +2,33 @@
 
 #Set Printer Name
 PRINTER="printer_shortname"
+PRINTER_UI_NAME="${PRINTER}"
+
+#Set Print Server or Printer Direct Host/IP
+NETPRINT="10.11.12.13"
+
+#Set Printer Location
+LOCATION="Print Room"
+
+#Set Printer Queue
+QUEUE="QUEUE%20NAME"
+
+#Set Driver Path
+DRIVER="/System/Library/Frameworks/ApplicationServices.framework/Versions/A/Frameworks/PrintCore.framework/Versions/A/Resources/Generic.ppd"
 
 #Remove Printer#
-/usr/sbin/lpadmin -x "$PRINTER"
+/usr/sbin/lpadmin -x "${PRINTER}"
 
 sleep 2
 
 #Add Printer via Print Server#
-#/usr/sbin/lpadmin -p "$PRINTER" -E -v smb://serverhost/"$PRINTER" -P /Library/Printers/PPDs/Contents/Resources/"$PRINTER".ppd -L "Location" -o auth-info-required=negotiate -o printer-is-shared=false
+#/usr/sbin/lpadmin -p "$PRINTER" -D "${PRINTER_UI_NAME}" -E -v smb://"$NETPRINT"/"${QUEUE}" -P "$DRIVER" -L "$LOCATION" -o auth-info-required=negotiate -o printer-is-shared=false
 
 #Add Printer via LPD#
-/usr/sbin/lpadmin -p "$PRINTER" -E -v lpd://127.0.0.1 -P /System/Library/Frameworks/ApplicationServices.framework/Versions/A/Frameworks/PrintCore.framework/Versions/A/Resources/Generic.ppd -L "Location" -o printer-is-shared=false
+#/usr/sbin/lpadmin -p "$PRINTER" -D "${PRINTER_UI_NAME}" -E -v lpd://"$NETPRINT"/"${QUEUE}" -P "$DRIVER" -L "$LOCATION" -o auth-info-required=negotiate -o printer-is-shared=false
 
+#Add Printer via IPP Everywhere/AirPrint
+/usr/sbin/lpadmin -p "$PRINTER" -D "${PRINTER_UI_NAME}" -E -v ipp://"${NETPRINT}"/ipp/print -m everywhere -L "$LOCATION" -o auth-info-required=negotiate -o printer-is-shared=false
 
 sleep 2
 
@@ -26,7 +41,7 @@ for USER_TEMPLATE in "/System/Library/User Template"/*
 
 for USER_HOME in /Users/*
   do
-    USER_UID=`basename "${USER_HOME}"`
+    USER_UID=$(basename "${USER_HOME}")
     if [ ! "${USER_UID}" = "Shared" ]
     then
       if [ ! -d "${USER_HOME}"/Library/Preferences ]
@@ -44,7 +59,7 @@ for USER_HOME in /Users/*
   done
 
 #Per User Default Printer Setting:
-    for uid in $(/usr/bin/dscl . -list /Users UniqueID | grep -Ev "^_|com.*|root|nobody|daemon|\/" | awk '{print $2}'); do
+    for uid in $(/usr/bin/dscl . -list /Users UniqueID | grep -Ev "^_|com.*|root|nobody|daemon|\\/" | awk '{print $2}'); do
         # Replace echo with e.g. launchctl load.
         launchctl asuser "$uid" chroot -u "$uid" / echo "Setting default printer for $uid"
         launchctl asuser "$uid" chroot -u "$uid" / lpoptions -d "$PRINTER"
