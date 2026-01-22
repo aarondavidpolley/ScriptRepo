4dbd69cda0d4de59170b8fe97357f7b04973864c
Aaron Polley <aarondavidpolley@gmail.com>
2026-01-22 12:05:00 +1000

    Update Printers-CUPS-Add-Printer.sh
    Added model option and removed code for setting default for each user which has errors in macOS 15/26

--- Printers-CUPS-Add-Printer.sh
+++ Printers-CUPS-Add-Printer.sh
@@ -13,8 +13,10 @@ LOCATION="Print Room"
 #Set Printer Queue
 QUEUE="QUEUE%20NAME"
 
-#Set Driver Path
+#Set Driver Path or Model
+#use lpinfo -lm to see installed models
 DRIVER="/System/Library/Frameworks/ApplicationServices.framework/Versions/A/Frameworks/PrintCore.framework/Versions/A/Resources/Generic.ppd"
+MODEL="drv:///sample.drv/generpcl.ppd"
 
 #Remove Printer#
 /usr/sbin/lpadmin -x "${PRINTER}"
@@ -22,7 +24,7 @@ DRIVER="/System/Library/Frameworks/ApplicationServices.framework/Versions/A/Fram
 sleep 2
 
 #Add Printer via Print Server#
-#/usr/sbin/lpadmin -p "$PRINTER" -D "${PRINTER_UI_NAME}" -E -v smb://"$NETPRINT"/"${QUEUE}" -P "$DRIVER" -L "$LOCATION" -o auth-info-required=negotiate -o printer-is-shared=false
+#/usr/sbin/lpadmin -p "$PRINTER" -D "${PRINTER_UI_NAME}" -E -v smb://"$NETPRINT"/"${QUEUE}" -m "$MODEL" -L "$LOCATION" -o auth-info-required=negotiate -o printer-is-shared=false
 
 #Add Printer via LPD#
 #/usr/sbin/lpadmin -p "$PRINTER" -D "${PRINTER_UI_NAME}" -E -v lpd://"$NETPRINT"/"${QUEUE}" -P "$DRIVER" -L "$LOCATION" -o auth-info-required=negotiate -o printer-is-shared=false
@@ -31,36 +33,3 @@ sleep 2
 /usr/sbin/lpadmin -p "$PRINTER" -D "${PRINTER_UI_NAME}" -E -v ipp://"${NETPRINT}"/ipp/print -m everywhere -L "$LOCATION" -o auth-info-required=negotiate -o printer-is-shared=false
 
 sleep 2
-
-#Set UseLastPrinter to False EVERYWHERE#
-
-for USER_TEMPLATE in "/System/Library/User Template"/*
-  do
-    /usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/org.cups.PrintingPrefs.plist UseLastPrinter -bool FALSE
-  done
-
-for USER_HOME in /Users/*
-  do
-    USER_UID=$(basename "${USER_HOME}")
-    if [ ! "${USER_UID}" = "Shared" ]
-    then
-      if [ ! -d "${USER_HOME}"/Library/Preferences ]
-      then
-        mkdir -p "${USER_HOME}"/Library/Preferences
-        chown "${USER_UID}" "${USER_HOME}"/Library
-        chown "${USER_UID}" "${USER_HOME}"/Library/Preferences
-      fi
-      if [ -d "${USER_HOME}"/Library/Preferences ]
-      then
-        /usr/bin/defaults write "${USER_HOME}"/Library/Preferences/org.cups.PrintingPrefs.plist UseLastPrinter -bool FALSE
-        chown "${USER_UID}" "${USER_HOME}"/Library/Preferences/org.cups.PrintingPrefs.plist
-      fi
-    fi
-  done
-
-#Per User Default Printer Setting:
-    for uid in $(/usr/bin/dscl . -list /Users UniqueID | grep -Ev "^_|com.*|root|nobody|daemon|\\/" | awk '{print $2}'); do
-        # Replace echo with e.g. launchctl load.
-        launchctl asuser "$uid" chroot -u "$uid" / echo "Setting default printer for $uid"
-        launchctl asuser "$uid" chroot -u "$uid" / lpoptions -d "$PRINTER"
-    done
