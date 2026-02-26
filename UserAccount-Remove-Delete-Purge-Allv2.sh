6ae22c65e95c2b394b69b452b03f3145b12f5a90
Aaron Polley <aarondavidpolley@gmail.com>
2026-02-26 14:01:24 +1000

    Update UserAccount-Remove-Delete-Purge-Allv2.sh

--- UserAccount-Remove-Delete-Purge-Allv2.sh
+++ UserAccount-Remove-Delete-Purge-Allv2.sh
@@ -24,4 +24,5 @@ echo "Deleting account and home directory for the following users..."
 for a in $userList ; do
             echo "$a"
             dscl . delete /Users/"$a"  #delete the account
+            rm -r /Users/"$a"  #delete the home directory
 done
